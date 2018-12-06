91a5e133f3fea97fb45cfe484817263f769a299b
Joe <pumphrey.joseph@gmail.com>
2018-12-06 11:14:54 -0800

    location cache

---
+++ schema.sql
@@ -0,0 +1,40 @@
+DROP TABLE IF EXISTS weathers;
+DROP TABLE IF EXISTS locaions;
+
+CREATE TABLE locations (
+id SERIAL PRIMARY KEY,
+search_query VARCHAR(255),
+formatted_query VARCHAR(255),
+latitude NUMERIC(8, 6),
+longitude  NUMERIC(8, 6)
+);
+
+CREATE TABLE weathers (
+id SERIAL PRIMARY KEY,
+forcast VARCHAR(255),
+time VARCHAR(255),
+location_id INTEGER NOT NULL,
+FOREIGN KEY (location_id) REFERENCES locations (id)
+);
+
+CREATE TABLE yelp(
+id SERIAL PRIMARY KEY,
+name VARCHAR(255),
+image_url VARCHAR(255),
+price VARCHAR(255),
+rating VARCHAR(255),
+url VARCHAR(255),
+location_id INTEGER NOT NULL,
+FOREIGN KEY (location_id) REFERENCES locations (id)
+);
+
+CREATE TABLE  moviedb (
+id SERIAL PRIMARY KEY,
+title VARCHAR(255),
+overview VARCHAR(255),
+total_votes VARCHAR(255),
+image_url VARCHAR(255),
+popularity VARCHAR(255),
+location_id INTEGER NOT NULL,
+FOREIGN KEY (location_id) REFERENCES locations (id)
+);
